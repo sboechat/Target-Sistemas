b5d163b5ac36c7a052da11692cffcd6b4b330650
Samuel Soares <42041748+sboechat@users.noreply.github.com>
2022-06-22 02:32:53 -0300

    Resposta do n° 1.

---
+++ um.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int INDICE = 13, SOMA = 0, K = 0;
+  while (K < INDICE){
+    K = K + 1;
+    SOMA = SOMA + K;
+  }
+  printf("%d\n", SOMA);
+  return 0;
+}
