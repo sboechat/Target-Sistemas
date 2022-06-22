587b0941e7db8cff1e5390ece01462b0c06a6bce
Samuel Soares <42041748+sboechat@users.noreply.github.com>
2022-06-22 06:31:55 -0300

    Update quatro.c

--- quatro.c
+++ quatro.c
@@ -12,12 +12,12 @@ double mathabs(double num){ //Retira o sinal negativo da distância relativa do 
 }
 
 int main(){
-    double msCarro = (110.0 / 3.6), msCaminhao = (-80.0 / 3.6); //Velocidade em metros por segundo. A velocidade do caminhão está relativa em direção ao ponto inicial (Ribeirão Preto).
     /*  - Sabemos que quando os dois automóveis se encontrarem, a soma das distâncias percorridas será de 100km, convertendo para metros dá 100.000m.
         - Com isso podemos calcular o tempo para o encontro de dois corpos (carro e caminhão).
         - Dividindo a distância percorrida pela velocidade do carro menos a velocidade relativa do caminhão, conseguimos o tempo gasto.
         - Se temos o tempo gasto para o encontro e sabemos a velocidade de ambos, podemos calcular a distância que cada um percorreu separadamente.
     */
+    double msCarro = (110.0 / 3.6), msCaminhao = (-80.0 / 3.6); //Velocidade em metros por segundo. A velocidade do caminhão está relativa em direção ao ponto inicial (Ribeirão Preto).
     double distCarroPedagio = (msCarro*2*5*60); //Calculando em m/s o percurso do carro enquanto o caminhão parou 10 minutos no pedágio.
     double t = (100000.0 - distCarroPedagio) / (msCarro - msCaminhao); //Calculando o tempo para o encontro (descontando a distância percorrida pelo carro enquanto o caminhão estava nos pedágios).
     double distCarro = (msCarro*t) + distCarroPedagio; //Calculando o tamanho do percurso (velocidade * tempo) que o carro percorreu, somando o que percorreu enquanto o caminhão estava parado.
